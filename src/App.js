d78e0a00eed6b47977ca2832fec5cd257c50cb7a
vivien-renee <vivien.renee.b@gmail.com>
2019-06-14 14:04:23 -0700

    connecting local repo to git repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components.js/Header';
+import MainContent from './components.js/MainContent';
+import Footer from './components.js/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+     
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components.js/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main>This is the Main Content</main>
+    );
+}
+
+export default MainContent;
